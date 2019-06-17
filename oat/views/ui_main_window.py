de5a1d7347b4d021f9ec8bc6ffcf9db3385951b6
Olivier Morelle <oli4morelle@gmail.com>
2019-06-18 00:25:48 +0200

    Improved views and added icons

---
+++ oat/views/ui_main_window.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_main_window.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(805, 575)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 805, 23))
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        self.menuImport = QtWidgets.QMenu(self.menuFile)
+        self.menuImport.setObjectName("menuImport")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.actionSave_as = QtWidgets.QAction(MainWindow)
+        self.actionSave_as.setObjectName("actionSave_as")
+        self.actionSave = QtWidgets.QAction(MainWindow)
+        self.actionSave.setObjectName("actionSave")
+        self.actionOpen_Project = QtWidgets.QAction(MainWindow)
+        self.actionOpen_Project.setObjectName("actionOpen_Project")
+        self.action_vol = QtWidgets.QAction(MainWindow)
+        self.action_vol.setEnabled(True)
+        self.action_vol.setObjectName("action_vol")
+        self.menuImport.addAction(self.action_vol)
+        self.menuFile.addAction(self.actionOpen_Project)
+        self.menuFile.addAction(self.menuImport.menuAction())
+        self.menuFile.addSeparator()
+        self.menuFile.addAction(self.actionSave_as)
+        self.menuFile.addAction(self.actionSave)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.menuImport.setTitle(_translate("MainWindow", "Import"))
+        self.actionSave_as.setText(_translate("MainWindow", "Save as"))
+        self.actionSave.setText(_translate("MainWindow", "Save"))
+        self.actionOpen_Project.setText(_translate("MainWindow", "Open Project"))
+        self.action_vol.setText(_translate("MainWindow", ".vol (HE raw Export)"))
+
+
